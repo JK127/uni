8b6dcecd46da73b87f9c447ce45fb59a140599c8
Jonas Klein <jonas.m.k94@gmail.com>
2018-06-21 18:46:39 +0200

    Insert optimiert

--- Datenbanken/src/CapDB.java
+++ Datenbanken/src/CapDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class CapDB {
 	private String user = "root";
 	private String password = "Pa$$w0rd";
-	private String connStr = "jdbc:mysql://localhost:3306/CAP_DB";
+	private String connStr = "jdbc:mysql://localhost:3306/CAP_DB?verifyServerCertificate=false&useSSL=true";
 	private String[] tables = { "customers", "products", "agents", "orders" };
 	private String[] createTables = {
 			"CREATE TABLE customers(\n" + "	cid int NOT NULL AUTO_INCREMENT,\n" + "	cname varchar(15) NOT NULL,\n"
@@ -24,8 +24,9 @@ public class CapDB {
 					+ "	qty bigint NOT NULL,\n" + "	dollars decimal(10,2) NOT NULL,\n" + "	PRIMARY KEY (oid)\n"
 					+ ");" };
 
-	// ALT: 348723
-	// NEU: 114953
+	// Pro Statement eine Connection: 348723
+	// Nur eine Connection: 114953
+	// Mit Batch: 15463
 	public static void main(String[] args) {
 		long start;
 		long ende;
@@ -65,41 +66,68 @@ public class CapDB {
 		Agent[] ag = getAgenten(agenten);
 		Customer[] cm = getCustomers(kunden);
 		Product[] pr = getProducts(produkte);
-		try (Connection conn = DriverManager.getConnection(connStr, user, password)) {
+
+		try (Connection conn = DriverManager.getConnection(connStr, user, password);
+				PreparedStatement ps = conn
+						.prepareStatement("INSERT INTO agents (aname, city, percent) VALUES (?, ?, ?);")) {
+
+			conn.setAutoCommit(false);
 
 			for (Agent a : ag) {
-				String insert = String.format("INSERT INTO agents (aname, city, percent) VALUES ('%s', '%s', %s);",
-						a.getName(), a.getCity(), a.getProzent());
-				try (PreparedStatement ps = conn.prepareStatement(insert)) {
-					ps.execute();
-				} catch (SQLException e) {
-					System.err.println(e);
-				}
+				ps.clearParameters();
+				ps.setString(1, a.getName());
+				ps.setString(2, a.getCity());
+				ps.setDouble(3, a.getProzent());
+				ps.addBatch();
 			}
 
-			for (Customer c : cm) {
-				String insert = String.format("INSERT INTO customers (cname, city, discnt) VALUES ('%s', '%s', %s);",
-						c.getName(), c.getCity(), c.getDsc());
-				try (PreparedStatement ps = conn.prepareStatement(insert)) {
-					ps.execute();
-				} catch (SQLException e) {
-					System.err.println(e);
-				}
-			}
+			ps.executeLargeBatch();
 
-			for (Product p : pr) {
-				String insert = String.format(
-						"INSERT INTO products (pname, city, quantity, price) VALUES ('%s', '%s', %s, %s);", p.getName(),
-						p.getCity(), p.getQuantity(), p.getPrice());
-				try (PreparedStatement ps = conn.prepareStatement(insert)) {
-					ps.execute();
-				} catch (SQLException e) {
-					System.err.println(e);
-				}
-			}
 		} catch (SQLException e) {
 			System.err.println(e);
 		}
+
+		try (Connection conn = DriverManager.getConnection(connStr, user, password);
+				PreparedStatement ps = conn
+						.prepareStatement("INSERT INTO customers (cname, city, discnt) VALUES (?, ?, ?);")) {
+
+			conn.setAutoCommit(false);
+
+			for (Customer c : cm) {
+				ps.clearParameters();
+				ps.setString(1, c.getName());
+				ps.setString(2, c.getCity());
+				ps.setDouble(3, c.getDsc());
+				ps.addBatch();
+			}
+
+			ps.executeLargeBatch();
+
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+
+		try (Connection conn = DriverManager.getConnection(connStr, user, password);
+				PreparedStatement ps = conn
+						.prepareStatement("INSERT INTO products (pname, city, quantity, price) VALUES (?, ?, ?, ?);")) {
+
+			conn.setAutoCommit(false);
+
+			for (Product p : pr) {
+				ps.clearParameters();
+				ps.setString(1, p.getName());
+				ps.setString(2, p.getCity());
+				ps.setDouble(3, p.getQuantity());
+				ps.setDouble(4, p.getPrice());
+				ps.addBatch();
+			}
+
+			ps.executeLargeBatch();
+
+		} catch (SQLException e) {
+			System.err.println(e);
+		}
+
 	}
 
 	private Agent[] getAgenten(int anz) {
